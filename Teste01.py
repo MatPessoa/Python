0a166fd2d53737013d4b71ecdafc0268e12e8552
Matheus <matheuspessoadeoliveira@outlook.com>
2023-06-21 14:34:22 -0300

    Primeiro commit

---
+++ Teste01.py
@@ -0,0 +1,8 @@
+nome = input ('Qual o seu nome?')
+idade = input ('Qual a sua idade?')
+peso = input ('Qual o seu peso?')
+print(nome, idade, peso)
+
+
+nome = input ('Qual o seu nome?')
+print ('Olá', nome,'! Prazer em te conhecer')
